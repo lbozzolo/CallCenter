800eb36d83aa0737e8845e5d17fcf76b17282f31
MAC <apple@MACs-MacBook-Pro.local>
2019-09-24 10:25:02 -0300

    Prueba de envío de mail

--- app/Http/Controllers/AlumnosController.php
+++ app/Http/Controllers/AlumnosController.php
@@ -65,7 +65,7 @@ class AlumnosController extends Controller
         }
 
         Mail::queue('emails.alta-coefix', ['data' => $data], function ($message) use ($data){
-            $message->to('test-4x0sg@mail-tester.com');
+            $message->to('test-wj4o5@mail-tester.com');
             $message->subject($data['subject']);
             $message->from('administracion@crm.coefix.com');
             $message->setContentType('text/plain; charset=UTF-8');
